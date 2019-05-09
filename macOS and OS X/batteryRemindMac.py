57cb598c51ad232819ac3e952afb32e410e05b50
coolkiwiii <47393358+coolkiwiii@users.noreply.github.com>
2019-05-09 12:16:57 -0400

    Added a bunch of files!

--- macOS and OS X/batteryRemindMac.py
+++ macOS and OS X/batteryRemindMac.py
@@ -1,10 +1,14 @@
 import psutil
 import pync
 import os
+from threading import Event
+import calendar
 import tkinter as tk
 from tkinter import messagebox
 
 battery = psutil.sensors_battery()
+discharging = True
+battpercent = battery.percent
 
 root = tk.Tk()
 root.withdraw()
@@ -13,26 +17,21 @@ def nohup():
     dirpath = os.path.realpath(__file__)
     os.system(f"nohup python3 {dirpath}")
 
-while battery.power_plugged == False:
-    for i in range(4):
-                if battery.percent == 60:
-                    if battery.percent == 50:
-                        if battery.percent == 40:
-                            if battery.percent == 30:
-                                pync.notify("Your battery is at 30%, please charge immediately.", appIcon="batteryhalf.png")
-                            else:
-                                pync.notify("Your battery is at 40%, please charge.", appIcon="batteryhalf.png")
-                        else:
-                            pync.notify("Your battery is at 50%, please charge.", appIcon="batteryhalf.png")
-                    else:
-                        pync.notify("Your battery is at 60%, please charge.", appIcon="batteryhalf.png")
+if battpercent == 60 and discharging == True:
+    pync.notify('Your battery is at 60%, please charge!',title="Battery Reminder")
+    Event().wait(timeout=60)
 
-while battery.power_plugged == True:
-    for i in range(1):
-        if battery.percent < 100:
-            pass
-        else:
-            pync.notify("Your battery fully charged! Feel free to unplug anytime!", appIcon="batteryfull.png")
+elif battpercent == 50 and discharging == True:
+    pync.notify('Your battery is at 50%, please charge!',title="Battery Reminder")
+    Event().wait(timeout=60)
+
+elif battpercent == 40 and discharging == True:
+    pync.notify('Your battery is at 40%, please charge!',title="Battery Reminder")
+    Event().wait(timeout=60)
+
+elif battpercent == 30 and discharging == True:
+    pync.notify('Your battery is at 30%, please charge!',title="Battery Reminder")
+    Event().wait(timeout=60)
 
 if not hasattr(psutil, "sensors_battery"):
     messagebox.showwarning('Sorry, but this platform is currently not supported.','Unsupported')
