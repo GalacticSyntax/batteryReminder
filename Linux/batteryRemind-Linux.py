385c75ac4b3ddbbdb997c9777f4dd5390d4fec04
coolkiwiii <47393358+coolkiwiii@users.noreply.github.com>
2019-05-11 13:37:40 -0400

    Updated python file

--- Linux/batteryRemind-Linux.py
+++ Linux/batteryRemind-Linux.py
@@ -2,6 +2,7 @@ import psutil
 import notify2
 import os
 import time
+import sys
 import tkinter as tk
 from tkinter import messagebox
 
@@ -15,6 +16,14 @@ root.withdraw()
 def nohup():
     dirpath = os.path.realpath(__file__)
     os.system(f"nohup python3 {dirpath}")
+
+if not hasattr(psutil, "sensors_battery"):
+    messagebox.showwarning('Sorry, but this platform is currently not supported.','Unsupported')
+    sys.exit()
+
+if battery is None:
+    messagebox.showerror('This program has detected that there is no battery installed on this computer. Click OK to close this window.','No Battery')
+    sys.exit()
 
 if battpercent == 60 and discharging == True:
     notify2.Notification('Battery Reminder',"Your battery is at 60%, please charge!")
@@ -42,9 +51,3 @@ elif battery.power_plugged == True:
     
 elif battery.power_plugged == False:
     discharging = True
-
-if not hasattr(psutil, "sensors_battery"):
-    messagebox.showwarning('Sorry, but this platform is currently not supported.','Unsupported')
-
-if battery is None:
-    messagebox.showerror('This program has detected that there is no battery installed on this computer. Click OK to close this window.','No Battery')
